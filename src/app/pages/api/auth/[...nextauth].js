df58ed5fe9de1b66e25cfa111698ec8eafb85402
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-01-08 21:21:45 +0800

    nextauth.js setup

--- src/app/pages/api/auth/[...nextauth].js
+++ src/app/pages/api/auth/[...nextauth].js
@@ -1,3 +1,20 @@
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../lib/mongodb';
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
+
+export default NextAuth(authOptions);
